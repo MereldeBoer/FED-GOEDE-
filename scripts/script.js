3df320e5a9df74cecec01c6a3d0071f59694cd68
MereldeBoer <deboermerel1@gmail.com>
2024-09-25 21:42:34 +0200

    ee

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,24 @@
 console.log("hi");
 
 const videojoris = document.getElementById('myVideo');
+let openButton = document.querySelector("header > button");
+let sluitButton = document.querySelector("nav button");
 
 videojoris.autoplay = true;
 videojoris.muted = true;
 videojoris.loop = true;
 
+openButton.onclick = openMenu;
+sluitButton.onclick = sluitMenu;
 
+function openMenu() {  
+    // zoek de nav op
+    let deNav = document.querySelector("nav");
+    // voeg class toe aan nav
+    deNav.classList.add("toonMenu");
+  }
+  function sluitMenu() {
+    let deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+ 
