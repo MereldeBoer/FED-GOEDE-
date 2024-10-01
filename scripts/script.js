1550d276f101ad676e746d1d6a7a5838967b87ca
MereldeBoer <deboermerel1@gmail.com>
2024-10-01 12:27:28 +0200

    c
    d

--- scripts/script.js
+++ scripts/script.js
@@ -41,23 +41,76 @@ function openMenu() {
     const infoElement = document.getElementById('info');
     const titleElement = document.getElementById('info-title');
     const textElement = document.getElementById('info-text');
-    const imageElement = document.getElementById('info-image');
+    const imageElement = document.getElementById('info-image'); 
+    
 
     const infoData = {
         joris: {
             title: "Joris en de Draak",
             text: "Een spannende rit waarbij je door een drakenhol gaat!",
-            image: "jorisdraak/baron1898.jpg"
+            image: "kaart/jorisendraak.jpg"
+           
+         
         },
         baron: {
             title: "Baron 1898",
             text: "Een achtbaan met een vrije val van 37,5 meter!",
-            image: "path/to/baron.jpg"
+            image: "kaart/baron1898.jpg"
         },
         symbolica: {
             title: "Symbolica",
             text: "Een betoverende dark ride voor het hele gezin.",
-            image: "path/to/symbolica.jpg"
+            image: "kaart/symbolica.jpg"
+        },
+        hollander: {
+            title: "Vliegende Hollander",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/vliegendehollander.jpg"
+        },
+        sprookjesbos: {
+            title: "Sprookjesbos",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/sprookjesbos.jpg"
+        },
+        droomvlucht: {
+            title: "Droomvlucht",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/droomvlucht.jpg"
+        },
+        python: {
+            title: "Python",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/python.jpg"
+        },
+        villavolta: {
+            title: "Villa Volta",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/villavolta.jpg"
+        },
+        pagode: {
+            title: "Pagode",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/pagode.jpg"
+        },
+        fatamorgana: {
+            title: "Fata Morgana",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/fatamorgana.jpg"
+        },
+        pirana: {
+            title: "Piraña",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/pirana.jpg"
+        },
+        carnaval: {
+            title: "Carnaval Festival",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/carnaval.jpg"
+        },
+        vogelrok: {
+            title: "Vogelrok",
+            text: "Een betoverende dark ride voor het hele gezin.",
+            image: "kaart/vogelrok.jpg"
         },
         // Voeg hier meer attracties toe met hun informatie
     };
